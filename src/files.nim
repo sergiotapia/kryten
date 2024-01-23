504b6816ae995f7ba7f936e92638e573f47c468b
Sergio Tapia <sergiotapiagutierrez@gmail.com>
2024-01-23 00:07:53 -0500

    Cut up documents by page

--- src/files.nim
+++ src/files.nim
@@ -1,4 +1,18 @@
 import std/osproc
+import std/strutils
 
-proc extractContents*(filepath: string): string =
-  execProcess("java -jar ./vendors/tika-app-3.0.0-BETA.jar -t " & repr(filepath))
+type
+  DocumentPage = object
+    text: string
+    page_number: int
+
+proc extractContents*(filepath: string): seq[DocumentPage] =
+  let raw_html = execProcess("java -jar ./vendors/tika-app-3.0.0-BETA.jar " & repr(filepath))
+  let body = raw_html.split("<body>")[1].split("</body>")[0]
+  let body_without_tags = body.replace("<p>", "").replace("</p>", "").replace("<div>", "").replace("</div>","").replace("<p />","").replace("<p/>","")
+  let text_pages = body_without_tags.split("""<div class="page">""")[1..^1]
+  var pages: seq[DocumentPage]
+  for page, text in text_pages.pairs:
+    pages.add(DocumentPage(text: text, page_number: page + 1))
+
+  return pages
--- src/kryten.nim
+++ src/kryten.nim
@@ -21,48 +21,48 @@ when isMainModule:
   if len(getEnv("OPENAI_API_KEY")) == 0:
     raise EnvironmentError.newException("Environment variable $OPENAI_API_KEY is not set.")
 
-  # var embeddings = generateEmbeddings("test")
-  # upsertEmbeddings("example.pdf", 1, "My name is Sergio", embeddings)
+  if len(getEnv("PINECONE_API_KEY")) == 0:
+    raise EnvironmentError.newException("Environment variable $PINECONE_API_KEY is not set.")
 
-  var questionEmbeddings = generateEmbeddings("What is your name?")
-  var ragContext = searchEmbeddings("example.pdf", questionEmbeddings)
-  echo %ragContext
-
-  let answer = answerQuestionWithRag("What is your name?", ragContext)
-  echo answer
-
-
+  if len(getEnv("PINECONE_ENDPOINT")) == 0:
+    raise EnvironmentError.newException("Environment variable $PINECONE_ENDPOINT is not set.")
 
   # if database.initNecessary():
   #   database.initDatabase()
 
-  # let doc = """
-  # Kryten
+  let doc = """
+  Kryten
 
-  # Usage:
-  #   kryten ask <filepath> question <question>
-  # """
+  Usage:
+    kryten ask <filepath> question <question>
+  """
     
-  # let args = docopt(doc, version = "Kryten 0.1.0")
+  let args = docopt(doc, version = "Kryten 0.1.0")
 
-  # if args["ask"] and args["question"]:
-  #   var filepath = $args["<filepath>"] 
-  #   var contents = extractContents(filepath)
-  
-  #   echo contents
-  #   echo filepath
+  if args["ask"] and args["question"]:
+    var filepath = $args["<filepath>"] 
+    var contents = extractContents(filepath)
+    echo contents
 
     # TODO: Check if embeddings exist for this filepath on Pinecone.
       
       # TODO: Chop up contents, generate embeddings, and insert
       # into Pinecone DB with metadata about the filepath.
+      # var embeddings = generateEmbeddings("test")
+      # upsertEmbeddings("example.pdf", 1, "My name is Sergio", embeddings)
 
     # TODO: Generate embeddings for the question, and query
     # Pinecone DB for the most similar embeddings where filepath
     # is the same.
+    # var questionEmbeddings = generateEmbeddings("What is your name?")
+    # var ragContext = searchEmbeddings("example.pdf", questionEmbeddings)
+    
 
     # TODO: Return the most similar embeddings to pass in as context.
+    # echo %ragContext
 
     # TODO: Send the matching context and the question to ChatGPT API.
+    # let answer = answerQuestionWithRag("What is your name?", ragContext)
+    # echo answer
 
     # TODO: Save question and answer for filepath in sqlite database.
